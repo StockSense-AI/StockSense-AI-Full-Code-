255d12894824d07e93dfa1f79f639b2442e87f9e
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2024-12-10 20:43:41 -0800

    Update and rename StockPrediction.py to StockPredictor.py

--- StockPredictor.py
+++ StockPredictor.py
@@ -3,14 +3,10 @@ import yfinance as yf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, r2_score
-from sklearn.metrics import mean_squared_error
-import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
+import numpy as np
 import datetime
-
-
-
 
 # Step 1: Fetch Stock Data
 def fetch_stock_data(ticker, start_date, end_date):
