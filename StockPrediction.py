64d37de8a016bff5dcd39188a8f40d50cfeb4c1d
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2024-12-10 20:37:34 -0800

    Update StockPrediction.py

--- StockPrediction.py
+++ StockPrediction.py
@@ -3,10 +3,11 @@ import yfinance as yf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error
+import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-
 
 
 
@@ -40,7 +41,7 @@ def train_model(X_train, y_train):
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     mae = mean_absolute_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     r2 = r2_score(y_test, y_pred)
     return y_pred, mae, rmse, r2
 
