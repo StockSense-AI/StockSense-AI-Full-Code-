00f408620df48ecc3fa6dc0ed1684c7d716bdac3
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2024-12-10 20:30:56 -0800

    Update StockPrediction.py

--- StockPrediction.py
+++ StockPrediction.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import datetime
-import joblib
+
 
 
 
